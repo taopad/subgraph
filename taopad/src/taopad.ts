5aa188a2bd8fc907c1dbf8affe199e06a85df385
niera26 <niera26@outlook.com>
2024-02-12 18:22:57 +0100

    first attempt

--- taopad/src/taopad.ts
+++ taopad/src/taopad.ts
@@ -1,168 +1,48 @@
-import {
-  AddToBlacklist as AddToBlacklistEvent,
-  Approval as ApprovalEvent,
-  Claim as ClaimEvent,
-  Distribute as DistributeEvent,
-  OptIn as OptInEvent,
-  OptOut as OptOutEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  RemoveFromBlacklist as RemoveFromBlacklistEvent,
-  Sweep as SweepEvent,
-  Transfer as TransferEvent
-} from "../generated/Taopad/Taopad"
-import {
-  AddToBlacklist,
-  Approval,
-  Claim,
-  Distribute,
-  OptIn,
-  OptOut,
-  OwnershipTransferred,
-  RemoveFromBlacklist,
-  Sweep,
-  Transfer
-} from "../generated/schema"
+import { Holder } from "../generated/schema"
+import { AddToBlacklist as AddToBlacklistEvent, Transfer as TransferEvent } from "../generated/Taopad/Taopad"
+import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleAddToBlacklist(event: AddToBlacklistEvent): void {
-  let entity = new AddToBlacklist(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.addr = event.params.addr
+  let entity = Holder.load(event.params.addr)
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (entity == null) {
+    entity = new Holder(event.params.addr)
+    entity.address = event.params.addr
+    entity.balance = BigInt.fromI32(0)
+  }
 
-  entity.save()
-}
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleClaim(event: ClaimEvent): void {
-  let entity = new Claim(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.addr = event.params.addr
-  entity.to = event.params.to
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleDistribute(event: DistributeEvent): void {
-  let entity = new Distribute(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.addr = event.params.addr
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOptIn(event: OptInEvent): void {
-  let entity = new OptIn(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.addr = event.params.addr
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOptOut(event: OptOutEvent): void {
-  let entity = new OptOut(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.addr = event.params.addr
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRemoveFromBlacklist(
-  event: RemoveFromBlacklistEvent
-): void {
-  let entity = new RemoveFromBlacklist(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.addr = event.params.addr
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSweep(event: SweepEvent): void {
-  let entity = new Sweep(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.addr = event.params.addr
-  entity.token = event.params.token
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.isBlacklisted = true
 
   entity.save()
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
+  let entityFrom = Holder.load(event.params.from)
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (entityFrom == null) {
+    entityFrom = new Holder(event.params.from)
+    entityFrom.address = event.params.from
+    entityFrom.balance = BigInt.fromI32(0)
+    entityFrom.isBlacklisted = false
+  }
 
-  entity.save()
+  let entityTo = Holder.load(event.params.to)
+
+  if (entityTo == null) {
+    entityTo = new Holder(event.params.to)
+    entityTo.address = event.params.to
+    entityFrom.balance = BigInt.fromI32(0)
+    entityTo.isBlacklisted = false
+  }
+
+  entityTo.balance = entityTo.balance.plus(event.params.value)
+
+  if (entityFrom.balance.lt(event.params.value)) {
+    entityFrom.balance = BigInt.fromI32(0)
+  } else {
+    entityFrom.balance = entityFrom.balance.minus(event.params.value)
+  }
+
+  entityFrom.save()
+  entityTo.save()
 }
